789b704a00679c8f4251669b2fe3086dcfed3d6a
Ethan Mathew <ethan.mathew55555@gmail.com>
2025-09-24 16:42:39 -0400

    Chapter 1 quiz

---
+++ Chapter1/quiz/q3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int first_num;
+    std::cin >> first_num;
+
+    std::cout << "Enter another integer: ";
+    int second_num;
+    std::cin >> second_num;
+
+    std::cout << first_num << " + " << second_num << " is " 
+        << (first_num + second_num) << ".\n";
+
+    std::cout << first_num << " - " << second_num << " is " 
+        << (first_num - second_num) << ".\n";
+
+    return 0;
+}
