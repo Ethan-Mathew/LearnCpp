c14f3dd1a5a94a767700bf8b4efc24ebb939335c
Ethan Mathew <ethan.mathew55555@gmail.com>
2025-09-30 19:29:03 -0400

    7.3

---
+++ Chapter7/7.3/q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int smaller;
+    std::cin >> smaller;
+
+    std::cout << "Enter a larger integer: ";
+    int larger;
+    std::cin >> larger;
+
+    if (smaller > larger)
+    {
+        int temp{smaller};
+        smaller = larger;
+        larger = temp;
+    }
+
+    std::cout << "The smaller value is: " << smaller << '\n';
+    std::cout << "The larger value is: " << larger << '\n';
+}
