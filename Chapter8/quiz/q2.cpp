3a446a7cdbc5c10aae58f430345731a1128a0e53
Ethan Mathew <ethan.mathew55555@gmail.com>
2025-10-05 12:41:54 -0400

    Chapter 8 quiz

---
+++ Chapter8/quiz/q2.cpp
@@ -0,0 +1,61 @@
+// Make sure that assert triggers even if we compile in release mode
+#undef NDEBUG
+
+#include <cassert> // for assert
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int x)
+{
+    double sqrtx{std::sqrt(x)};
+
+    if (x == 0 || x == 1)
+    {
+        return false;
+    }
+
+    for (int i{2}; i < x; ++i)
+    {
+        if (i % 2 == 0 && i != 2)
+        {
+            continue;
+        }
+
+        if (x % i == 0)
+        {
+            return false;
+        }
+
+        if (i + 1 > sqrtx)
+        {
+            break;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    assert(!isPrime(0)); // terminate program if isPrime(0) is true
+    assert(!isPrime(1));
+    assert(isPrime(2));  // terminate program if isPrime(2) is false
+    assert(isPrime(3));
+    assert(!isPrime(4));
+    assert(isPrime(5));
+    assert(isPrime(7));
+    assert(!isPrime(9));
+    assert(isPrime(11));
+    assert(isPrime(13));
+    assert(!isPrime(15));
+    assert(!isPrime(16));
+    assert(isPrime(17));
+    assert(isPrime(19));
+    assert(isPrime(97));
+    assert(!isPrime(99));
+    assert(isPrime(13417));
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
