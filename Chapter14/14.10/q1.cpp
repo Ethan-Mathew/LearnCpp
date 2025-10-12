74c7971548427e0310d9261d92925d7d3c0f2c50
Ethan Mathew <ethan.mathew55555@gmail.com>
2025-10-12 18:19:47 -0400

    14.10

---
+++ Chapter14/14.10/q1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string_view>
+
+class Ball
+{
+public:
+    Ball(std::string_view colour, double radius)
+        : m_colour {colour}, m_radius {radius}
+    {
+    }
+
+    const std::string& getColour() const
+    {
+        return m_colour;
+    }
+
+    double getRadius() const
+    {
+        return m_radius;
+    }
+
+private:
+    std::string m_colour;
+    double m_radius {};
+};
+
+void print(const Ball& ball)
+{
+    std::cout << "Ball(" << ball.getColour() << ", " << ball.getRadius() << ")\n";
+}
+
+int main()
+{
+	Ball blue { "blue", 10.0 };
+	print(blue);
+
+	Ball red { "red", 12.0 };
+	print(red);
+
+	return 0;
+}
