d6d90119db3e7a3c1a6e988cc618223e5c68883b
Ethan Mathew <ethan.mathew55555@gmail.com>
2025-10-11 23:38:57 -0400

    13.2

---
+++ Chapter13/13.2/q1.cpp
@@ -0,0 +1,8 @@
+enum MonsterType
+{
+    orc,
+    goblin,
+    troll,
+    ogre,
+    skeleton,
+};
---
+++ Chapter13/13.2/q2.cpp
@@ -0,0 +1,18 @@
+namespace MonsterType
+{
+    enum MonsterType
+    {
+        orc,
+        goblin,
+        troll,
+        ogre,
+        skeleton,
+    };
+}
+
+int main()
+{
+    MonsterType::MonsterType monster{MonsterType::troll};
+
+    return 0;
+}
