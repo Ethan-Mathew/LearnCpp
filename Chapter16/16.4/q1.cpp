98ff525fa1c739c452fd79fe8f7d6ec931c24f42
Ethan Mathew <ethan.mathew55555@gmail.com>
2025-10-13 13:54:43 -0400

    16.4

---
+++ Chapter16/16.4/q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printElement(const std::vector<T>& vec, int index)
+{
+    if (index >= std::size(vec))
+    {
+        std::cout << "Invalid index\n";
+        return;
+    }
+
+    std::cout << "The element has value: " << vec.at(index) << '\n';
+}
+
+int main()
+{
+    std::vector v1 { 0, 1, 2, 3, 4 };
+    printElement(v1, 2);
+    printElement(v1, 5);
+
+    std::vector v2 { 1.1, 2.2, 3.3 };
+    printElement(v2, 0);
+    printElement(v2, -1);
+
+    return 0;
+}
